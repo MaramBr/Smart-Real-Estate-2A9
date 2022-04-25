fd3786fa064dc0df7eb1f21e5c2fb548406ad6c6
yoserfrini <yosr.frini@esprit.tn>
2022-04-25 21:29:10 +0100

    interface

---
+++ english.ts
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en" sourcelanguage="fr">
+<context>
+    <name>Dialog_ajout</name>
+    <message>
+        <location filename="dialog_ajout.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="26"/>
+        <source>ajouter/modifier une maison</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="78"/>
+        <source> id_appartement</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="91"/>
+        <source>    prix</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="117"/>
+        <source>description </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="130"/>
+        <source>adresse</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="143"/>
+        <source>ajouter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="156"/>
+        <source>Image_maison</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="dialog_ajout.ui" line="169"/>
+        <source>modifier</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="35"/>
+        <source>Gestion des appartements</source>
+        <translation>apartment management</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <source>Ajouter un appartement</source>
+        <translation>add an apartment</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="158"/>
+        <source> id appartement</source>
+        <translation> apartment id</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="171"/>
+        <source>   description</source>
+        <translation>   description</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="184"/>
+        <source>   prix</source>
+        <translation>   price</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="197"/>
+        <source>nb_chambres</source>
+        <translation>rooms number</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="210"/>
+        <source>id_immeuble</source>
+        <translation>building_id</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="249"/>
+        <source>image</source>
+        <translation>picture</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="282"/>
+        <source>Ajouter</source>
+        <translation>add</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="314"/>
+        <source>Modifier</source>
+        <translation>modify</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="346"/>
+        <source>Image</source>
+        <translation>picture</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="363"/>
+        <source>Supprimer un appartement</source>
+        <translation>delete an apartment</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="388"/>
+        <location filename="mainwindow.ui" line="453"/>
+        <source>id appartement</source>
+        <translation>apartment id</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="421"/>
+        <source>Supprimer</source>
+        <translation>delete</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="438"/>
+        <source>Rechercher un appartement</source>
+        <translation>search an apartment</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="499"/>
+        <source>Rechercher</source>
+        <translation>search</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="516"/>
+        <source>Liste des appartements</source>
+        <translation>list of apartments</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="558"/>
+        <source> PDF</source>
+        <translation> PDF</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="575"/>
+        <source>Trier</source>
+        <translation>sort</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="587"/>
+        <source>    prix croissant</source>
+        <translation>    ascending price</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="600"/>
+        <source>    prix decroissant</source>
+        <translation>    descending price</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="659"/>
+        <source>Quitter</source>
+        <translation>Exit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="666"/>
+        <source>stat</source>
+        <translatorcomment>statistic</translatorcomment>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="671"/>
+        <source>Page</source>
+        <translation>page</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="758"/>
+        <source>Statistiques</source>
+        <translation>statistics</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="165"/>
+        <source>choose an image </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="165"/>
+        <source>All files (*)</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="appartements.cpp" line="140"/>
+        <location filename="appartements.cpp" line="161"/>
+        <location filename="appartements.cpp" line="175"/>
+        <source>id_appartement</source>
+        <translation>apartment_id</translation>
+    </message>
+    <message>
+        <location filename="appartements.cpp" line="141"/>
+        <location filename="appartements.cpp" line="162"/>
+        <location filename="appartements.cpp" line="176"/>
+        <source>prix</source>
+        <translation>price</translation>
+    </message>
+    <message>
+        <location filename="appartements.cpp" line="142"/>
+        <location filename="appartements.cpp" line="163"/>
+        <location filename="appartements.cpp" line="177"/>
+        <source>nb_chambres</source>
+        <translation>rooms_number</translation>
+    </message>
+    <message>
+        <location filename="appartements.cpp" line="143"/>
+        <location filename="appartements.cpp" line="164"/>
+        <location filename="appartements.cpp" line="178"/>
+        <source>description_A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="appartements.cpp" line="144"/>
+        <location filename="appartements.cpp" line="165"/>
+        <location filename="appartements.cpp" line="179"/>
+        <source>id_immeuble</source>
+        <translation>building_id</translation>
+    </message>
+    <message>
+        <location filename="appartements.cpp" line="145"/>
+        <source>image</source>
+        <translation>picture</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="26"/>
+        <source>database is open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="27"/>
+        <source>connection successful.
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="32"/>
+        <source>database is not open</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="33"/>
+        <source>connection failed.
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="61"/>
+        <location filename="mainwindow.cpp" line="110"/>
+        <source> ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
+        <source>ajout effecte  .
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="69"/>
+        <location filename="mainwindow.cpp" line="118"/>
+        <source>not ok</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="70"/>
+        <source>ajout non effecte .
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="111"/>
+        <source>modif effecte  .
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="119"/>
+        <source>modif non effecte .
+Click Cancel to exit.</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
